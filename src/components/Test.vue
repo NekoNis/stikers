9e8daea822d3abc32b8af150e52b322cdef95e85
NekoNis <fopjo@yandex.ru>
2023-10-07 10:21:45 +0300

    Init

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { ref } from 'vue'
+  const count = ref(1)
+</script>
+
+<template>
+  <input v-model="count" style="margin-right: 10px">
+  <button @click="count++" style="margin-right: 10px">
+    Сумматор
+  </button>
+  <span>Cчёт: {{ count }}</span>
+</template>
+
+<style scoped>
+
+</style>
