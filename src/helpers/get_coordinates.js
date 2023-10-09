f5c8d5a0669aae2de55f3b8044254c2e80c45dd8
NekoNis <fopjo@yandex.ru>
2023-10-09 13:13:45 +0300

    Функция рассчёта позиций квадратов одинакового размера.

---
+++ src/helpers/get_coordinates.js
@@ -0,0 +1,52 @@
+// const x_list = 230; // px
+// const y_list = 130; // or 1000 px
+// const space = 5;
+// const list_size = [['1', 100, 100], ['2', 100, 100], ['3', 100, 100], ['4', 100, 100], ['5', 100, 100], ['6', 100, 100], ['7', 100, 100]];
+
+// let test = [
+//     [[['name_stikers'], ['size_x'], ['size_y']], ...], // картинки
+//     [['list_size_x'], ['list_size_y'], ['space']] // хар_лист
+// ]
+
+
+export function get_pos(space, x_list, y_list, list_size) {
+    let coordinates = [];
+    if (y_list === 0) {
+        let overload_x = space;
+        let overload_y = space;
+        let i = 0;
+        while (i < list_size.length) {
+            let x = overload_x;
+            overload_x += space + list_size[i][1];
+            if (overload_x < x_list) {
+                coordinates.push([list_size[i][0], x, overload_y]);
+                i++;
+            } else {
+                overload_y += space + list_size[i][2];
+                overload_x = space;
+            }
+        }
+    } else {
+        let number_list = 1;
+        let overload_x = space;
+        let overload_y = space;
+        let i = 0;
+        while (i < list_size.length) {
+            let x = overload_x;
+            let y = overload_y;
+            overload_x += space + list_size[i][1];
+            if (overload_x < x_list) {
+                coordinates.push([list_size[i][0], x, y, number_list]);
+                i++;
+            } else if (overload_y + list_size[i][2] * 2 < y_list) {
+                overload_x = space;
+                overload_y += space + list_size[i][2];
+            } else {
+                overload_x = space;
+                overload_y = space;
+                number_list += 1;
+            }
+        }
+    }
+    return coordinates
+}
